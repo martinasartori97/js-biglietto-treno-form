8e90f3e31330d877b4d919b37e500115a6eb3e19
martinasartori97 <martinasartori9@gmail.com>
2024-10-09 16:43:33 +0200

    edit js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 console.log("hello");
+
+// chiedere all'utente  il numero di chilometri che vuole percorrere
+let chilometri = prompt("quanti chilometri vuoi percorrere?");
+chilometri = Number(chilometri);
+console.log(80);
+console.log(chilometri);
+
+
+// chiedere l'età del passeggero
+let eta = prompt("qual è la tua età?");
+console.log(26);
+eta = Number(eta);
+console.log(eta);
+
+// prezzo per chilometro
+const prezzoalkm = 0.21;
+
+// calcolo del prezzo totale
+let prezzototale = chilometri * prezzoalkm
+
+// applicare uno sconto del 20% sui minorenni
+// applicare uno sconto del 20% sui minorenni e del 40% per gli over 65
+if eta(< 18:
+    sconto = 0.20   Sconto del 20 % per i minorenni)
+   eta(> 65:
+        sconto = 0.40   Sconto del 40 % per gli over 65)
+
+else (sconto = 0.0   Nessuno sconto)
+// applicare uno sconto del 40% per gli over 65
+
+// prezzo finale
+function formatprice(price) {
+    return `${price.tofixed()
+        }`
+}
